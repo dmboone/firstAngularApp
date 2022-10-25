ca90594aa8b80ea1e9b12f00599470d39341cd42
Destiny Boone <destaj.db@gmail.com>
2022-10-25 16:32:00 -0400

    string interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css'],
-  styles: [` /** can also do inline styling in the decorator! */
+  styles: [` /** can also do inline styling in the decorator instead of linking to separate css file */
     h3{
       color: dodgerblue;
     }
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,8 +5,18 @@ import { Component } from "@angular/core"; // must import this to use Component 
 
 @Component({ // this is a decorator; we will list some configurations for our component in here
     selector: 'app-server', // this refers to the the html tag by which we are able to use this component later
+                            // can also change the selector to be based on class '.app-server' or attribute '[app-server]'
     templateUrl: './server.component.html' // this links us to the html file that will be referenced when we create this component
 })
 export class ServerComponent {
+    serverId = 10;
+    serverStatus = 'offline';
 
+    getServerStatus(){
+        return this.serverStatus;
+    }
+
+    // can also write it this way
+    // serverId: number = 10;
+    // serverStatus: string = 'offline';
 }
