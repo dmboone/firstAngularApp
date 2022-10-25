71704148d11ddad6149de8612c6163b3f05534a3
Destiny Boone <destaj.db@gmail.com>
2022-10-25 16:42:15 -0400

    property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,15 +4,21 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',
   // you can inline your html by using template instead of templateURL!
   // if you decide not to do inline html here, then just make sure you use the templateURL instead and link to separate html file
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>
-  `,
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>
+  // `,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
 
-  constructor() { }
+  constructor() { // method that is executed at the point of time this component is created
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    }, 2000)
+  }
 
   ngOnInit(): void {
   }
