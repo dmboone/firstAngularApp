5192ccdb2b50fdd5288f19535d83f09a7555988d
Destiny Boone <destaj.db@gmail.com>
2022-10-25 13:57:10 -0400

    how to create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component'; // make sure to import your created components; don't need the .ts, this gets added automatically by webpack when bundled
+import { ServersComponent } from './servers/servers.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent // list your created components here
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+// Practice with how to create our own components
+// Make sure that you go and list your component inside app.module.ts to actually use it!
+
+import { Component } from "@angular/core"; // must import this to use Component decorator
+
+@Component({ // this is a decorator; we will list some configurations for our component in here
+    selector: 'app-server', // this refers to the the html tag by which we are able to use this component later
+    templateUrl: './server.component.html' // this links us to the html file that will be referenced when we create this component
+})
+export class ServerComponent {
+
+}
