c7f037e5735faf14b4cc838154726c644f96cbfe
Destiny Boone <destaj.db@gmail.com>
2022-10-25 14:12:18 -0400

    component styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css'],
+  styles: [` /** can also do inline styling in the decorator! */
+    h3{
+      color: dodgerblue;
+    }
+  `]
 })
 export class AppComponent {
   
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  // you can inline your html by using template instead of templateURL!
+  // if you decide not to do inline html here, then just make sure you use the templateURL instead and link to separate html file
+  template: `
+  <app-server></app-server>
+  <app-server></app-server>
+  `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
