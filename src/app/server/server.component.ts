296bc0932e3208a58e7a48bad38326536514e113
Destiny Boone <destaj.db@gmail.com>
2022-10-26 14:29:29 -0400

    styling elements dynamically with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -11,12 +11,19 @@ import { Component } from "@angular/core"; // must import this to use Component 
 export class ServerComponent {
     serverId = 10;
     serverStatus = 'offline';
+    // can also write it this way
+    // serverId: number = 10;
+    // serverStatus: string = 'offline';
+
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
 
     getServerStatus(){
         return this.serverStatus;
     }
 
-    // can also write it this way
-    // serverId: number = 10;
-    // serverStatus: string = 'offline';
+    getColor(){
+        return this.serverStatus === 'online' ? 'green' : 'red';
+    }
 }
